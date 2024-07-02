b22037f58179724f1b63afab60f3b75e8d524a84
s1300252 <166572149+s1300252@users.noreply.github.com>
2024-07-03 01:06:35 +0900

    Final revision

--- hot.py
+++ hot.py
@@ -1,5 +1,8 @@
 import random
 
+name = input("Who are you?\n> ")
+print(f"Hello, {name}!")
+
 print("Tossing a coin...")
 
 results = []
@@ -12,3 +15,8 @@ heads_count = results.count("Heads")
 tails_count = results.count("Tails")
 
 print(f"Heads: {heads_count}, Tails: {tails_count}")
+
+if heads_count > tails_count:
+    print(f"{name} won!")
+else:
+    print(f"{name} lost!")
